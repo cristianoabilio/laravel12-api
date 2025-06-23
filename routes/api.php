e17ec602a7be27c5a8eb4ffec19bb5910c2f504e
Cristiano Abilio <cristianocafr@gmail.com>
2025-06-23 11:52:25 -0300

    added breeze package

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\API\StudentsApiController;
+use App\Http\Controllers\API\TestApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+// First API Route for testing
+
+Route::get('/test', [TestApiController::class, 'test'])->name('test-api');
+
+Route::apiResource('/students', StudentsApiController::class);
