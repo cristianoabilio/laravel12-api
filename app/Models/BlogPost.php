ce5c669499ef3ca6cbfc65db9b0f6fc53214e482
Cristiano Abilio <cristianocafr@gmail.com>
2025-06-26 15:03:00 -0300

    model migration and controller

---
+++ app/Models/BlogPost.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BlogPost extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'category_id',
+        'title',
+        'content',
+        'except',
+        'thumbnail',
+        'status',
+        'published_at'
+    ];
+}
